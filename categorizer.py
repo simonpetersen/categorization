bdf3e19b948b20433a81796360a1dae10e09e460
Rasmus Blumensaadt <rabl@netcompany.com>
2018-12-06 13:13:54 +0100

    Map Reduce for All articles

--- categorizer.py
+++ categorizer.py
@@ -1,7 +1,7 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 from utils.db import DbConnection
-
+import os
 
 class MRCategorizer(MRJob):
 
@@ -37,6 +37,70 @@ class MRCategorizer(MRJob):
         return [MRStep(mapper=self.mapper, combiner=self.combiner_count_words,
                        reducer=self.reducer_remove_stopwords),
                 MRStep(reducer=self.reducer_find_categories)]
+
+
+class MRCategorizerAll(MRJob):
+
+    db = DbConnection("utils/WikiDB.sqlite")
+    ignored_words = db.getIgnoreList()
+
+    def mapper(self, _, line):
+        filename = os.environ["map_input_file"]
+        for word in line.split():
+            yield filename + ':' + word.lower(), 1
+
+    def combiner_count_words(self, key, values):
+        yield key, sum(values)
+
+    def reducer_remove_stopwords(self, key, values):
+        keyword = key.split(':')[1]
+        if keyword not in MRCategorizer.ignored_words:
+            yield None, (key, sum(values))
+
+    def reducer_find_categories(self, _, word_counts):
+        sorted_counts = sorted(list(word_counts), key=lambda w: w[1], reverse=True)
+        count_treshold = 20
+        sorted_counts2 = list(filter(lambda c: c[1] >= count_treshold, sorted_counts))
+        categories = []
+        merged_categories = []
+        for count in sorted_counts2:
+            word = count[0].split(':')[-1]
+            matches_shorter = list(filter(lambda c: c[0].split(':')[-1][:len(word)] == word, categories))
+            matches_longer = list(filter(lambda c: c[0].split(':')[-1] == word[:len(c[0].split(':')[-1])], categories))
+            if len(matches_shorter) > 0 or len(matches_longer) > 0:
+                merged_matches = matches_longer + matches_shorter
+                # Finds the files associated with this category
+                files_in_matches = merged_matches[0][0].split(':')[:-1]
+                insert = False
+                for i in files_in_matches:
+                    if count[0].split(':')[0] == i:
+                        insert = False
+                        break
+                    insert = True
+                #Insert the new count into categories with the new filename appended to it
+                if (insert):
+                    count[0] = count[0].split(':')[0] + (':' + merged_matches[0][0])
+                    #Removes old entry of category
+                    index = categories.index(merged_matches[0])
+                    del categories[index]
+                else:
+                    continue
+            categories.append(count)
+            if(len(count[0].split(':')[:-1]) > 1):
+                for category in merged_categories:
+                    if(category[0].split(':')[-1] == count[0].split(':')[-1]):
+                        index = merged_categories.index(category)
+                        del merged_categories[index]
+                merged_categories.append(count)
+            if len(merged_categories) == 10:
+                return merged_categories
+        return merged_categories + categories[:10 - len(merged_categories)]
+
+    def steps(self):
+        return [MRStep(mapper=self.mapper, combiner=self.combiner_count_words,
+                       reducer=self.reducer_remove_stopwords),
+                MRStep(reducer=self.reducer_find_categories)]
+
 
 
 if __name__ == '__main__':
--- runner.py
+++ runner.py
@@ -1,33 +1,71 @@
 import utils.xmlparser
 from categorizer import MRCategorizer
+from categorizer import MRCategorizerAll
 from utils.db import DbConnection
 
 connection = DbConnection("utils/WikiDB.sqlite")
 list_of_articles = connection.getAllXmlFiles()
 article_names = list(map(lambda a: a[1], list_of_articles))
+article_names = article_names + ['All']
 print('#### Available articles:')
 for n in article_names:
     print(n)
 print('#####')
 line = input('Write the name of the article you want to categorize: ')
-while line not in article_names:
+while line.lower() not in [x.lower() for x in article_names]:
     line = input('Invalid article. Please try again: ')
 
-##### Extract text from xml
-file = list(filter(lambda a: a[1] == line, list_of_articles))[0][0]
-parser = utils.xmlparser.XmlParser(file)
-text = parser.parse()
-print("Article %s parsed." % line)
+if(line.lower() == 'all'):
+    files = []
+    for entry in list_of_articles:
+        parser = utils.xmlparser.XmlParser(entry[0])
+        text = parser.parse()
+        filelocation = "text-files/Parsed_Files/content." + entry[1] + ".txt"
+        file = open(filelocation, 'w', encoding="UTF-8")
+        file.write(text)
+        file.close
+        files.append(filelocation)
+    print("All Articles parsed.")
+    job = MRCategorizerAll(args=files)
+    with job.make_runner() as runner:
+        runner.run()
+        counter = 1
+        print('Following categorization can be suggested for the articles')
+        for key, value in job.parse_output(runner.cat_output()):
+            category_name = key.split(':')[-1]
+            list_of_files = key.split(':')[:-1]
+            list_of_names = []
+            for file in list_of_files:
+                filename = file.split('.')[1:-1]
+                list_of_names.append(filename)
+            category_spaces =  ' ' * int(2 - int(counter/10))
+            print(str(counter) + category_spaces + 'Category: ' + category_name)
+            for i in list_of_names:
+                print(' | ' + i[0], end = '')
+            print(' | ')
+            counter = counter + 1
 
-##### Write text to content-file
-filelocation = "text-files/content.txt"
-file = open(filelocation, 'w')
-file.write(text)
-file.close()
 
-##### Generate categories
-job = MRCategorizer(args=[filelocation])
-with job.make_runner() as runner:
-    runner.run()
-    for key, value in job.parse_output(runner.cat_output()):
-        print(key)
+else:
+    ##### Extract text from xml
+    file = list(filter(lambda a: a[1].lower() == line.lower(), list_of_articles))[0][0]
+    parser = utils.xmlparser.XmlParser(file)
+    text = parser.parse()
+    print("Article %s parsed." % line)
+
+    ##### Write text to content-file
+    filelocation = "text-files/content.txt"
+    file = open(filelocation, 'w', encoding="UTF-8")
+    file.write(text)
+    file.close()
+
+    ##### Generate categories
+    job = MRCategorizer(args=[filelocation])
+    with job.make_runner() as runner:
+        runner.run()
+        print('The following categories can be suggested for this article:')
+        counter = 1
+        for key, value in job.parse_output(runner.cat_output()):
+            category_spaces =  ' ' * int(2 - int(counter/10))
+            print(str(counter) + ':' + category_spaces + key)
+            counter = counter + 1
